02419b865070fc880678fe9460b5a02ac8e3415f
Zach Stearns <zach.stearns92@gmail.com>
2020-09-10 10:27:29 -0600

    Add lotto contestants method

--- lib/museum.rb
+++ lib/museum.rb
@@ -24,4 +24,26 @@ class Museum
   def admit(patron)
     @patrons << patron
   end
+
+
+
+  # def patrons_by_exhibit_interest
+  #   # @exhibits.each do |exhibit|
+  #   # @patrons.each do |patron|
+  #     @exhibits.group_by do |exhibit|
+  #       @patrons.each do |patron|
+  #         recommend_exhibits(patron)
+  #       end
+  #     end
+  # end
+
+  def ticket_lottery_contestants(exhibit)
+    list = []
+    @patrons.each do |patron|
+      if patron.interests.include?(exhibit.name) && patron.spending_money < exhibit.cost
+        list << patron
+      end
+    end
+    list
+  end
 end
