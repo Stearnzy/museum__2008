64c087cc0a6cad1f5a2fa88ff72aa144852f3b68
Zach Stearns <zach.stearns92@gmail.com>
2020-09-10 10:53:36 -0600

    WIP, cannot figure out stub

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/patron'
 require './lib/exhibit'
 require './lib/museum'
@@ -113,8 +114,6 @@ class MuseumTest < Minitest::Test
     dmns.admit(patron_2)
     dmns.admit(patron_3)
 
-    # require "pry"; binding.pry
-
     expected = { gems_and_minerals => [patron_1],
                 dead_sea_scrolls => [patron_1, patron_2, patron_3],
                 imax => [] }
@@ -147,5 +146,64 @@ class MuseumTest < Minitest::Test
     dmns.admit(patron_3)
 
     assert_equal [patron_1, patron_3], dmns.ticket_lottery_contestants(dead_sea_scrolls)
+  end
+
+  def test_pick_lotto_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    dmns.stub(:rand,1) do
+      assert_equal "Bob", dmns.draw_lottery_winner(dead_sea_scrolls)
+    end
+  end
+
+  def test_announce_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    dmns.stub(:rand,1) do
+      assert_equal "Bob", dmns.draw_lottery_winner(dead_sea_scrolls)
+    end
+    assert_equal "Bob has won the Dead Sea Scrolls exhibit lottery", dmns.announce_lottery_winner(dead_sea_scrolls)
   end
 end
